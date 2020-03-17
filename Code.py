b30ed5e0c8b452ba0be3b4a0192c30dbdaf81217
Naveen Kumar <59390882+Naveenkumar-cmd@users.noreply.github.com>
2020-03-17 10:47:13 +0530

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,13 @@
+one = int(input())
+two = int(input())
+three = int(input())
+if one == two:
+   print("0")
+elif one==three:
+   print("0")
+elif two==three:
+   print("0")
+elif two==one:
+   print("0")
+else:
+   print("1")
